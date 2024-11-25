f27a0bcfa04f3d3aa6a67e49752730960fe31367
Grette Thel <grethellagulao@yahoo.com>
2024-11-25 14:52:14 +0100

    Commit 1: Added UserInputValidator class with validate_positive_integers method and extra_info.ty with class description

---
+++ main.py
@@ -0,0 +1,10 @@
+class UserInputValidator:
+    def validate_positive_integers(self, input_list):
+        valid_integers = []
+        for item in input_list:
+            if item.isdigit() and int(item) > 0:
+                valid_integers.append(int(item))
+        return valid_integers
+
+    def display_message(self):
+        print("List has been successfully validated!")
