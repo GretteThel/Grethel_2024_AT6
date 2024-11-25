dea54865ee31fa4235fbb234811b24860d269c62
Grette Thel <grethellagulao@yahoo.com>
2024-11-25 15:23:48 +0100

    Commit 3: Updated instances.py to call display_messsage and validate integers

--- instances.py
+++ instances.py
@@ -4,6 +4,13 @@ from main import UserInputValidator
 validator1 = UserInputValidator()
 validator2 = UserInputValidator()
 
-# Example
-print(validator1.validate_positive_integers(["12", "-4", "0", "45", "abc"]))
-print(validator2.validate_positive_integers(["100", "xyz", "34", ""]))
+# Example usage
+result1 = validator1.validate_positive_integers(["12", "-4", "0", "45", "abc"])
+result2 = validator2.validate_positive_integers(["100", "xyz", "34", ""])
+
+# Display results
+validator1.display_message()
+print("Validator 1 Results:", result1)
+
+validator2.display_message()
+print("Validator 2 Results:", result2)
